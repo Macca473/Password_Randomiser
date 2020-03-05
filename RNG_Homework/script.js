4502e5e7ded5544f4ce0589a2dbef155412c2f5c
Matthew <mmacca473@gmail.com>
2020-03-06 10:16:19 +1030

    first push, still figuring stuff out

---
+++ RNG_Homework/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+var rand = Math.random().toString(30).substring(7, 5);
+
+console.log("random", rand);
+
+
+var rand2 = Math.random().toString(30);
+
+console.log("random2", rand2);
+
+var char = "abcdefg"
+
+var lengthinput = 7
+
+function lengthboi(lengmulti) {
+
+        var lengmulti = 0
+    for (var lni = lengthinput; lni > 0; lni--) {
+        lengmulti = lengthinput * 10;
+    }
+
+    return lengmulti;
+}
+
+var mulitylpr = lengthboi.lengmulti
+
+console.log("leng", mulitylpr);
+
+var rand3 = char.charAt(Math.floor(Math.random()   ));
+
+console.log("random3", rand3)
+
+
