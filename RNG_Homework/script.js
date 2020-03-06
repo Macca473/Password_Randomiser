49ec4871bd9ba8bd2f62ea273b305b20eddd151a
Matthew <mmacca473@gmail.com>
2020-03-06 13:34:58 +1030

    added the length prompt

--- RNG_Homework/script.js
+++ RNG_Homework/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+///////////////////////////// Testing cr*p
 
 var rand = Math.random().toString(30).substring(7, 5);
 
@@ -17,7 +18,7 @@ var rand3 = char.charAt(Math.floor(Math.random()   ));
 
 console.log("random3", rand3)
 
-//////////////////////////////
+////////////////////////////// refrence code
 
 function makeid(length) {
     var result           = '';
@@ -33,36 +34,61 @@ function makeid(length) {
 
  /////////////////////////////
 
+ //var leng = 7
+
+ //var uppercase = "yes"
+
+ //var lowercase = "yes"
+
+ //var numbers = "yes"
+
+ //var specialcharacters = "yes"
+
+//length choose - multiplyer REDUNDANT and doesn't work anyway 
+
+ //function rndmlt(multipr) {
+   
+
+    //for ( var n = 0; n < leng; n++) {
+         //multipr = multipr * 10;
+    //}
+    //return multipr;
+ //}
+
+ //var mtpr = rndmlt(1);
+
+ //console.log(mtpr);
+
+//
+
+//length choose - unnessery restrictions 
+
+var lengraw = prompt("how long do you want the password? (needs to be a number within 8 and 128 characters");
+
+var lengfn = function lengfn() {
+      var leng = 0;
+   if (lengraw >= 8, lengraw <= 128) {
+         leng = lengraw;
+   } else {
+      lengraw = alert("can't you read? it needs to be needs to be a number within 8 and 128");
+   }
+   return leng;
+}
+
+   var leng = lengfn();
+
+   console.log("leng", leng)
 
 
- var leng = 7
+//character choices
 
- var uppercase = "yes"
+var uppercase = "yes"
 
  var lowercase = "yes"
 
  var numbers = "yes"
 
  var specialcharacters = "yes"
-
-//length choose - multiplyer
-
- function rndmlt(multipr) {
-   
-
-    for ( var n = 0; n < leng; n++) {
-         multipr = multipr * 10;
-    }
-    return multipr;
- }
-
- var mtpr = rndmlt(1);
-
- console.log(mtpr);
-
-//
-
-//character choices
 
 var charchoose = function charchoose() {
       var uprcs = ""
@@ -94,10 +120,22 @@ var char = charchoose();
 
 //
 
-// everything thrown together
+//
 
 console.log("uppercase test", char);
 
-console.log("lil test",Math.floor(Math.random() * rndmlt(1)));
+console.log("lil test",Math.floor(Math.random() * 1));
 
+//
 
+//everything thrown together
+
+var randpsw = function randpsw() {
+         var rndcrd = "";
+      for (var x = 0; leng > x; x++) {
+         rndcrd = rndcrd + char.charAt(Math.floor(Math.random() * char.length));
+      }
+      return rndcrd;
+}
+
+console.log("final?", randpsw());
